134ec06d9356d095942c8bc23fa6eff4fd571ba6
purwokang <purwo.com@gmail.com>
2020-12-25 17:36:11 +0700

    fungsi GET sudah OK semua

--- application/controllers/api/Mahasiswa.php
+++ application/controllers/api/Mahasiswa.php
@@ -13,12 +13,28 @@ class Mahasiswa extends REST_Controller
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('Mahasiswa_model');
+        $this->load->model('Mahasiswa_model', 'mahasiswa');
     }
 
     public function index_get()
     {
-        $mahasiswa = $this->Mahasiswa_model->getMahasiswa();
-        var_dump($mahasiswa);
+        $id = $this->get('id');
+        if ($id === null) {
+            $mahasiswa = $this->mahasiswa->getMahasiswa();
+        } else {
+            $mahasiswa = $this->mahasiswa->getMahasiswa($id);
+        }
+
+        if ($mahasiswa) {
+            $this->response([
+                'status' => true,
+                'data' => $mahasiswa
+            ], REST_Controller::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'id not found'
+            ], REST_Controller::HTTP_NOT_FOUND);
+        }
     }
 }
--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -2,8 +2,12 @@
 
 class Mahasiswa_model extends CI_Model
 {
-    public function getMahasiswa()
+    public function getMahasiswa($id = null)
     {
-        return $this->db->get('mahasiswa')->result_array();
+        if ($id === null) {
+            return $this->db->get('mahasiswa')->result_array();
+        } else {
+            return $this->db->get_where('mahasiswa', ['id' => $id])->result_array();
+        }
     }
 }
